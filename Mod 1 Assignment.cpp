c6f631d4d9c4f9d891cf0845da46ad0f07b020c5
NCurtisSchool <nathan.curtis@csuglobal.edu>
2024-12-15 22:34:22 -0500

    Add files via upload

---
+++ Mod 1 Assignment.cpp
@@ -0,0 +1,20 @@
+/*
+Name: Nathan Curtis
+Date: 12/15/2024
+Class: Data Structures and Algorithms 24WB-CSC400-1
+Teacher: Herbert Pensado
+Assignment: Mod 1 Assignment
+GitHub: https://github.com/NCurtisSchool/Programing3Mod1.git
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "First Name: Amanda" << endl;
+    cout << "Last Name: Monna" << endl;
+    cout << "Street Address: 99 E Something Street" << endl;
+    cout << "City: Some City" << endl;
+    cout << "Zip Code: 99999" << endl;
+    return 0;
+}
